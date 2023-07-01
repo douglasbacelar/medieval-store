825b06f26e1153c7e645dcdcd57be2a2393d8a61
Douglas Bacelar <bacelardouglas@outlook.com>
2023-07-01 16:27:53 -0300

    Finalizando outros testes de integração do endpoint login

--- tests/integration/login/login.test.ts
+++ tests/integration/login/login.test.ts
@@ -22,4 +22,46 @@ describe('POST /login', function () {
     expect(httpResponse.status).to.equal(200);
     expect(httpResponse.body).to.have.key('token');
   })
+
+  it('Não recebeu um username, retornou um erro e status 400', async function () {
+    const httpBodyResquest = userMock.noUsernameLoginBody;
+
+    const httpResponse = await chai.request(app).post('/login').send(httpBodyResquest);
+
+    expect(httpResponse.status).to.equal(400);
+    expect(httpResponse.body).to.be.deep.equal({ message: '"username" and "password" are required' });
+});
+
+it('Não recebeu uma senha, retornou um erro e status 400', async function () {
+  const httpBodyResquest = userMock.noPasswordLoginBody
+
+  const httpResponse = await chai.request(app).post('/login').send(httpBodyResquest);
+
+  expect(httpResponse.status).to.equal(400);
+  expect(httpResponse.body).to.be.deep.equal({ message: '"username" and "password" are required' });
+});
+
+it('Recebeu um username inexistente, retorne um erro', async function () {
+  const httpBodyResquest = userMock.notExistingUserBody
+  sinon.stub(UserModel, 'findOne').resolves(null);
+
+  const httpResponse = await chai.request(app).post('/login').send(httpBodyResquest);
+
+  expect(httpResponse.status).to.equal(401);
+  expect(httpResponse.body).to.be.deep.equal({ message: 'Username or password invalid' });
+});
+
+it('ao receber um e-mail existente e uma senha errada, retorne um erro', async function () {
+
+  const httpBodyResquest = userMock.existingUserWithWrongPasswordBody 
+  const mockFindOneReturn = UserModel.build(userMock.existingUser);
+  sinon.stub(UserModel, 'findOne').resolves(mockFindOneReturn);
+
+
+  const httpResponse = await chai.request(app).post('/login')
+    .send(httpBodyResquest);
+
+  expect(httpResponse.status).to.equal(401);
+  expect(httpResponse.body).to.be.deep.equal({ message: 'Username or password invalid' });
+});
 });
